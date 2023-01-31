cc6e799dccfe002d1f09bc597a054db9fb5e27f7
Johanneslueke <Johanneslueke@users.noreply.github.com>
2023-01-31 17:32:31 +0100

    1 file modified

--- intervals/src/app/services/interval-checker.service.spec.ts
+++ intervals/src/app/services/interval-checker.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { firstValueFrom, Observable, of, tap } from 'rxjs';
 
 import { IntervalCheckerService } from './interval-checker.service';
 
@@ -13,4 +14,46 @@ describe('IntervalCheckerService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  const intervalData = [
+    // {
+    // 	start: of(['01.01.2023 00:00', '02.01.2023 00:00'] as string[]),
+    // 	duration: of([1, 1] as number[]),
+    // 	expected: [],
+    // },
+    {
+      start: of(['01.01.2023 00:00', '01.01.2023 00:30'] as string[]),
+      duration: of([1, 1] as number[]),
+      expected: [
+        {
+          index: 0,
+          overlapsWith: 1,
+          Interval: {
+            start: Date.parse('01.01.2023 00:00'),
+            end: Date.parse('01.01.2023 00:00') + 1,
+            duration: 1,
+          },
+        },
+      ],
+    },
+  ];
+
+  it.each(intervalData)(
+    'should detect overlaps',
+    async ({
+      start,
+      duration,
+      expected,
+    }: {
+      start: Observable<Array<string>>;
+      duration: Observable<Array<number>>;
+      expected: any;
+    }) => {
+      const actual = service.detect(duration, start).pipe(tap(console.error));
+
+      await expect(firstValueFrom(actual)).resolves.toEqual(
+        expect.arrayContaining(expected)
+      );
+    }
+  );
 });
