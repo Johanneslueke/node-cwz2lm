414a533bdafdb4ff0d05b171979133cb2784c803
Johanneslueke <Johanneslueke@users.noreply.github.com>
2023-01-31 19:00:09 +0100

    1 file modified

--- intervals/src/app/services/interval-checker.service.spec.ts
+++ intervals/src/app/services/interval-checker.service.spec.ts
@@ -38,10 +38,36 @@ describe('IntervalCheckerService', () => {
     },
   ];
 
-  it('should detect overlaps', async () => {
+  it('should detect overlaps ONE', async () => {
     const sourceA = cold('--x|', { x: [1, 1] });
     const sourceB = cold('--x|', {
       x: ['01.01.2023 00:00', '01.01.2023 00:00'],
+    });
+    const expected = cold('--x|', {
+      x: [
+        {
+          index: 0,
+          overlapsWith: 1,
+          // Interval: {
+          //   start: Date.parse('01.01.2023 00:00'),
+          //   end: Date.parse('01.01.2023 00:00') + 1,
+          //   duration: 1,
+          // },
+        },
+        {
+          index: 1,
+          overlapsWith: 0,
+        },
+      ],
+    });
+
+    expect(service.detect(sourceB, sourceA)).toBeObservable(expected);
+  });
+
+  it('should detect overlaps TWO', async () => {
+    const sourceA = cold('--x|', { x: [1000 * 60, 1000 * 60] });
+    const sourceB = cold('--x|', {
+      x: ['01.01.2023 00:00', '01.01.2023 00:00:30'],
     });
     const expected = cold('--x|', {
       x: [
